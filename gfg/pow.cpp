4d1bc4d0167d7bf148bcd681e1c77f0a66dc9a7f
Arup3201 <arupjana7365@gmail.com>
2026-03-29 21:59:08 +0530

    x power n calculation in constant time

---
+++ gfg/pow.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int pow(int x, int n)
+{
+    int pow = 1, curr = x;
+    for (int i = 0; i < 32; i++)
+    {
+        if (n & 1 << i)
+            pow *= curr;
+        curr *= curr;
+    }
+
+    return pow;
+}
+// ---------------- RUN SOLUTION ----------------
+vector<int> runSolution(istream &in)
+{
+    int t;
+    in >> t;
+
+    vector<int> results;
+
+    while (t--)
+    {
+        int x, n;
+        in >> x >> n;
+
+        results.push_back(pow(x, n));
+    }
+
+    return results;
+}
+
+// ---------------- READ EXPECTED OUTPUT ----------------
+vector<int> readExpected(const string &filename)
+{
+    ifstream file(filename);
+    vector<int> expected;
+    string line;
+
+    while (getline(file, line))
+    {
+        if (!line.empty())
+            expected.push_back(stoi(line));
+    }
+
+    return expected;
+}
+
+// ---------------- COMPARE OUTPUT ----------------
+void compareOutputs(const vector<int> &actual, const vector<int> &expected)
+{
+    int n = max(actual.size(), expected.size());
+
+    for (int i = 0; i < n; i++)
+    {
+        int a = (i < actual.size()) ? actual[i] : -1;
+        int e = (i < expected.size()) ? expected[i] : -1;
+
+        if (a == e)
+        {
+            cout << "Test " << i + 1 << ": PASS\n";
+        }
+        else
+        {
+            cout << "Test " << i + 1 << ": FAIL\n";
+            cout << "  Expected: " << e << "\n";
+            cout << "  Got     : " << a << "\n";
+        }
+    }
+}
+
+// ---------------- MAIN ----------------
+int main()
+{
+    // Redirect input from file
+    ifstream inputFile("input.txt");
+
+    if (!inputFile)
+    {
+        cerr << "Error opening input.txt\n";
+        return 1;
+    }
+
+    // Run solution
+    vector<int> actualOutput = runSolution(inputFile);
+
+    // Read expected output
+    vector<int> expectedOutput = readExpected("output.txt");
+
+    // Compare
+    compareOutputs(actualOutput, expectedOutput);
+
+    return 0;
+}
